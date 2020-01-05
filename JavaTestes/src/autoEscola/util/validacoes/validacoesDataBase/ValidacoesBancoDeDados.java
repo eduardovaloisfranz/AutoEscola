402186d1b56c4520c33160a62172f578da2224e8
EduardoValoisFranz <38699565+eduardovaloisfranz@users.noreply.github.com>
2020-01-05 00:19:44 -0300

    correção validarAulaCarro()
    permitia inserir aula de carro em aluna de moto ja existente, corrgido

--- JavaTestes/src/autoEscola/util/validacoes/validacoesDataBase/ValidacoesBancoDeDados.java
+++ JavaTestes/src/autoEscola/util/validacoes/validacoesDataBase/ValidacoesBancoDeDados.java
@@ -90,7 +90,7 @@ public class ValidacoesBancoDeDados {
 
     public static boolean validarAulaCarro(Aula aula, String cpfInstrutor) {
         String SQL = "SELECT COUNT(a.dataAulaInicio)'qtdAulaInicio', COUNT(b.cpf)'qtdInstrutor', COUNT(a.dataAulaTermino)'qtdAulaTermino' FROM aula a"
-                + " JOIN instrutor B ON a.fk_instrutor = b.id WHERE (? BETWEEN a.dataAulaInicio AND a.dataAulaTermino) OR (?  BETWEEN a.dataAulaInicio AND a.dataAulaTermino) AND (a.modalidadeAula = 'Carro') AND (b.cpf = ?)";
+                + " JOIN instrutor B ON a.fk_instrutor = b.id WHERE ((? BETWEEN a.dataAulaInicio AND a.dataAulaTermino) OR (?  BETWEEN a.dataAulaInicio AND a.dataAulaTermino)) AND (b.cpf = ?)";
         Connection conexao = FabricaConexao.getConnection();
         PreparedStatement stmt = null;
         ResultSet resultado = null;
