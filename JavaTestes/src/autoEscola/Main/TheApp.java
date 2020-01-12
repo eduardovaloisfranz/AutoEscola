09e455795355b8692e996c2a5b7545e8b45cd23f
EduardoValoisFranz <38699565+eduardovaloisfranz@users.noreply.github.com>
2020-01-12 00:15:55 -0300

    tentativa de fazer interface grafica pela primeira vez em java

--- JavaTestes/src/autoEscola/Main/TheApp.java
+++ JavaTestes/src/autoEscola/Main/TheApp.java
@@ -4,15 +4,17 @@ import autoEscola.controller.AlunoController;
 import autoEscola.controller.Menu;
 import autoEscola.util.validacoes.validaCPF.ValidaCPF;
 import autoEscola.util.validacoes.validacoesDataBase.ValidacoesBancoDeDados;
+import autoEscola.view.CadastroInstrutor;
 
 public class TheApp {
 
-    public static void main(String[] args) {        
-        Menu.menu();        
+   // public static void main(String[] args) {        
+        //Menu.menu();        
+        //CadastroInstrutor tela = new CadastroInstrutor
         //AlunoController.exibirTodasAulasAluno();
        //AlunoController.exibirTodosOsAlunosAceitamTroca();
         //AlunoController.exibirTodosOsAlunosNaoAceitamTroca();
        // AlunoController.exibirAulasAluno("10118916971");
        //AlunoController.exibirTodasAulasAlunoAceitamTroca();
-    }
+    //}
 }
